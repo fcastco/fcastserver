ed79fbcaad1c29dfa3c9bc70efc1ea4c46257367
FCast <admin@fcast.co>
2019-06-09 02:46:09 +0000

    Add jinja2 templates

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from sanic import Sanic
 from sanic.response import json
+from sanic_jinja2 import SanicJinja2
+
 
 app = Sanic()
+jinja = SanicJinja2(app)
 
 @app.route("/")
 async def test(request):
-    return json({"hello": "world"})
+    return jinja.render("index.html", request)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
