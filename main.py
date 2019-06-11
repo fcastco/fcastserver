54d2cf1298a9ac11d28e6077e3d8afb121c697df
Anil Makhijani <anil@Anils-MacBook-Pro.local>
2019-06-11 15:40:34 -0400

    Read from configuration file.

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from sanic_jinja2 import SanicJinja2
 
 app = Sanic()
 jinja = SanicJinja2(app)
+app.config.from_envvar('MYAPP_SETTINGS')
 
 @app.route("/")
 async def test(request):
+    print(app.config.UPLOAD_DIR)
     return jinja.render("index.html", request)
 
 if __name__ == "__main__":
