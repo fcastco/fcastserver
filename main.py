748ae32a3eeb7a76466ce2923627768db0cf922f
Anil Makhijani <admin@fcast.co>
2019-06-20 19:09:50 -0400

    Save user_id and pass user_id and filenames to the template.

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from sanic_jinja2 import SanicJinja2
 from sanic_motor import BaseModel
 from sanic_session import Session, MongoDBSessionInterface
 from controllers.file import file_bp
+from uuid import uuid4
 
 app = Sanic()
 app.blueprint(file_bp)
 jinja = SanicJinja2(app)
 app.config.from_envvar('MYAPP_SETTINGS')
+app.static('/d', './downloads')
 
 BaseModel.init_app(app)
 session = Session(app, interface=MongoDBSessionInterface(app))
@@ -21,7 +23,15 @@ session = Session(app, interface=MongoDBSessionInterface(app))
 
 @app.route("/", methods=["GET"])
 async def front_page(request):
-    return jinja.render("index.html", request)
+    try:
+        user_id = request["session"]["user_id"]
+    except KeyError:
+        user_id = request["session"]["user_id"] = str(uuid4())
+    try:
+        filenames = request["session"]["filenames"]
+    except KeyError:
+        filenames = {}
+    return jinja.render("index.html", request, user_id=user_id, filenames=filenames)
 
 
 if __name__ == "__main__":
