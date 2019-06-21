1e14aac627f2df6a91d525653a16ee7140a3dd9d
Anil Makhijani <admin@fcast.co>
2019-06-21 18:50:46 -0400

    Save files to session; create zip.

--- controllers/file.py
+++ controllers/file.py
@@ -5,8 +5,12 @@ import os
 from sanic.response import redirect
 from sanic import Blueprint
 import aiofiles
+from uuid import uuid4
 
 file_bp = Blueprint('file_bp')
+
+ALLOWED_FILE_EXTENSIONS = ["pdf", "csv"]
+ALLOWED_FILE_TYPES = ["application/pdf", "text/csv"]
 
 async def write_file(path, body):
     async with aiofiles.open(path, 'wb') as f:
@@ -15,8 +19,9 @@ async def write_file(path, body):
 
 
 def valid_file_type(file_name, file_type):
-    file_name_type = file_name.split('.')[-1]
-    if file_name_type == "pdf" and file_type == "application/pdf":
+    file_name_type = file_name.split('.')[-1].lower()
+    if file_name_type in ALLOWED_FILE_EXTENSIONS and\
+            file_type in ALLOWED_FILE_TYPES:
         return True
     return False
 
@@ -49,6 +54,44 @@ def secure_filename(filename):
     return filename
 
 
+def add_file_to_session(filename, request):
+    try:
+        request['session']['filenames']
+    except KeyError:
+        request['session']['filenames'] = {}
+    file_uuid = str(uuid4())
+    request['session']['filenames'].update({file_uuid: filename})
+    return
+
+
+@file_bp.route("file/zip", methods=["POST"])
+def create_zip(request):
+    user_id = request.form.get("user_id")
+    pw = request.form.get("zip_password")
+    download_dir = request.app.config.DOWNLOAD_DIR
+    # Create upload folder if doesn't exist
+    if not os.path.exists(download_dir):
+        os.makedirs(upload_dir)
+    upload_dir = request.app.config.UPLOAD_DIR + "/" + user_id
+    import subprocess
+    subprocess.call(['zip', '-j', '-r', '-P'+pw, download_dir + "/" + user_id + '.zip', upload_dir])
+    return redirect("/d/"+user_id+".zip")
+
+
+@file_bp.route("file/delete", methods=["POST"])
+def rm_file_from_session(request):
+    upload_dir = request.app.config.UPLOAD_DIR
+    user_id = request.form.get("user_id")
+    file_uuid = request.form.get("file_uuid")
+    print(request.form)
+    filename = request['session']['filenames'][file_uuid]
+    file_path = upload_dir + "/" + user_id + "/" + filename
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    del request['session']['filenames'][file_uuid]
+    return redirect('/?deleted_file')
+
+
 @file_bp.route("/upload", methods=["POST"])
 async def process_upload(request):
 
@@ -56,6 +99,11 @@ async def process_upload(request):
     # Create upload folder if doesn't exist
     if not os.path.exists(upload_dir):
         os.makedirs(upload_dir)
+
+    user_id = request['session']['user_id']
+    user_dir = upload_dir + "/" + user_id
+    if not os.path.exists(user_dir):
+        os.makedirs(user_dir)
 
     # Ensure a file was sent
     upload_file = request.files.get('file_names')
@@ -72,14 +120,7 @@ async def process_upload(request):
     elif not valid_file_size(upload_file.body):
         return redirect('/?error=invalid_file_size')
     else:
-        file_path = f"{upload_dir}/{filename}"
+        file_path = f"{user_dir}/{filename}"
+        add_file_to_session(filename, request)
         await write_file(file_path, upload_file.body)
-        try:
-            filenames = request['session']['filenames']
-        except KeyError:
-            filenames = []
-            request['session']['filenames'] = filenames
-        filenames.append(filename)
-        request['session']['filenames'] = filenames
         return redirect('/?error=none')
-
