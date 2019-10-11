2da5a2908ea8e2619cfb91bfe187657a2da35bc9
Anil Makhijani <admin@fcast.co>
2019-10-11 18:33:44 -0400

    Update code so it can interact with react front end rather than just from its own index forms.

--- controllers/file.py
+++ controllers/file.py
@@ -3,14 +3,24 @@ import os
 
 # 3rd party packages
 from sanic.response import redirect
+from sanic.response import file_stream
+from sanic_motor import BaseModel
 from sanic import Blueprint
+from sanic.response import text
 import aiofiles
 from uuid import uuid4
+from pymongo import ReturnDocument
 
 file_bp = Blueprint('file_bp')
 
 ALLOWED_FILE_EXTENSIONS = ["pdf", "csv"]
 ALLOWED_FILE_TYPES = ["application/pdf", "text/csv"]
+
+
+class User(BaseModel):
+    __coll__ = 'users'
+    __unique_fields__ = ['user_id']
+
 
 async def write_file(path, body):
     async with aiofiles.open(path, 'wb') as f:
@@ -61,21 +71,48 @@ def add_file_to_session(filename, request):
         request['session']['filenames'] = {}
     file_uuid = str(uuid4())
     request['session']['filenames'].update({file_uuid: filename})
-    return
+    return file_uuid
 
 
-@file_bp.route("file/zip", methods=["POST"])
-def create_zip(request):
+async def get_user(user_id):
+    user = await User.find_one_and_update(
+        {'user_id':user_id},
+        {'$set':{'user_id':user_id}},
+        upsert=True,
+        return_document=ReturnDocument.AFTER
+    )
+    return user
+        
+
+async def add_file_to_db(user, filename):
+    filenames = user.get("filenames", {})
+    file_uuid = str(uuid4())
+    filenames.update({file_uuid: filename})
+    user = await User.update_one({'user_id':user['user_id']}, {'$set':{'filenames':filenames}})
+    return file_uuid
+
+
+@file_bp.route("file/zip", methods=["POST", "OPTIONS"])
+async def create_zip(request):
     user_id = request.form.get("user_id")
     pw = request.form.get("zip_password")
+    redi = request.form.get("redirect", False)
     download_dir = request.app.config.DOWNLOAD_DIR
     # Create upload folder if doesn't exist
     if not os.path.exists(download_dir):
         os.makedirs(upload_dir)
     upload_dir = request.app.config.UPLOAD_DIR + "/" + user_id
     import subprocess
-    subprocess.call(['zip', '-j', '-r', '-P'+pw, download_dir + "/" + user_id + '.zip', upload_dir])
-    return redirect("/d/"+user_id+".zip")
+    filename = user_id + '.zip'
+    subprocess.call(['zip', '-j', '-r', '-P'+pw, download_dir + "/" + filename, upload_dir])
+    if redi:
+        return redirect("/d/"+filename)
+    return await file_stream(
+        "downloads/" + filename,
+        headers={'Content-type': 'application/zip', 'Content-Disposition': 'attachment', 'filename': '"'+filename+'"'},
+        filename=filename
+    )
+    #return await file_stream(request.app.config.HOST_NAME+"d/"+user_id+".zip")
 
 
 @file_bp.route("file/delete", methods=["POST"])
@@ -83,7 +120,6 @@ def rm_file_from_session(request):
     upload_dir = request.app.config.UPLOAD_DIR
     user_id = request.form.get("user_id")
     file_uuid = request.form.get("file_uuid")
-    print(request.form)
     filename = request['session']['filenames'][file_uuid]
     file_path = upload_dir + "/" + user_id + "/" + filename
     if os.path.exists(file_path):
@@ -92,18 +128,66 @@ def rm_file_from_session(request):
     return redirect('/?deleted_file')
 
 
-@file_bp.route("/upload", methods=["POST"])
-async def process_upload(request):
-
+def create_dirs(request, user_id=None):
     upload_dir = request.app.config.UPLOAD_DIR
     # Create upload folder if doesn't exist
     if not os.path.exists(upload_dir):
         os.makedirs(upload_dir)
 
-    user_id = request['session']['user_id']
+    if not(user_id):
+        user_id = request['session']['user_id']
     user_dir = upload_dir + "/" + user_id
     if not os.path.exists(user_dir):
         os.makedirs(user_dir)
+    return
+
+
+@file_bp.route('/api/v1/file/process/<user_id>', methods=['DELETE'])
+async def process_file_api_delete(request, user_id):
+    user = await get_user(user_id)
+    upload_dir = request.app.config.UPLOAD_DIR
+    file_uuid = request.body
+    filenames = user["filenames"]
+    filename = filenames[file_uuid.decode()]
+    del filenames[file_uuid.decode()]
+    user = await User.update_one({'user_id':user['user_id']}, {'$set':{'filenames':filenames}})
+    file_path = upload_dir + "/" + user_id + "/" + filename
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    else:
+        print("could not remove " + file_path)
+    return text("success")
+
+
+@file_bp.route('/api/v1/file/process/<user_id>', methods=['POST', 'OPTIONS'])
+async def process_file_api_post(request, user_id):
+    create_dirs(request, user_id)
+    upload_dir = request.app.config.UPLOAD_DIR
+    user_dir = upload_dir + "/" + user_id
+    file_fp = request.files.get('filepond')
+    if not file_fp:
+        return text("No file found.")
+    elif not valid_file_type(file_fp.name, file_fp.type):
+        return text("Invalid file type.")
+    elif not valid_file_size(file_fp.body):
+        return text("File too large.")
+    else:
+        filename = secure_filename(file_fp.name)
+        file_path = f"{user_dir}/{filename}"
+        user = await get_user(user_id)
+        file_uuid = await add_file_to_db(user, filename)
+        await write_file(file_path, file_fp.body)
+        return text(file_uuid)
+
+
+@file_bp.route("/upload", methods=["POST"])
+async def process_upload(request):
+
+    upload_dir = request.app.config.UPLOAD_DIR
+    # Create upload folder if doesn't exist
+    user_id = request['session']['user_id']
+    user_dir = upload_dir + "/" + user_id
+    create_dirs(request)
 
     # Ensure a file was sent
     upload_file = request.files.get('file_names')
